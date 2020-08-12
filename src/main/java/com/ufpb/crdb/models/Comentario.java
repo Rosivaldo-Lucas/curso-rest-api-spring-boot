ba42b5a473b0145a463b144b233c7427bbdd684f
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2020-08-12 13:02:00 -0300

    start do projeto e criação das entidades

---
+++ src/main/java/com/ufpb/crdb/models/Comentario.java
@@ -0,0 +1,36 @@
+package com.ufpb.crdb.models;
+
+import java.time.OffsetDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Comentario {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @EqualsAndHashCode.Include
+  private Long id;
+
+  private String comentario;
+
+  @Column(name = "created_at")
+  private OffsetDateTime createdAt;
+
+  @Column(name = "updated_at")
+  private OffsetDateTime updatedAt;
+
+}
---
+++ src/test/java/com/ufpb/crdb/CrdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpb.crdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
